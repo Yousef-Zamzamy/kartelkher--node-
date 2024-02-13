965cbaaf2bb5ecfde2b7815449949be4df82b9e8
yousef zamzamy <yousefzamzamy1191@gmail.com>
2024-02-13 16:53:09 +0200

    view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
--- public/js/sdkahview.js
+++ public/js/sdkahview.js
@@ -33,17 +33,3 @@ const summaryfn = () => {
   });
 };
 summaryfn();
-const exit = document.getElementById("exit")
-const exit2 = document.getElementById("exit2")
-exit.addEventListener("click" , (e) => {
-  window.history.pushState(null, "", location.href = "/");
-  window.onpopstate = function(){
-    window.history.pushState(null, "", location.href = "/");
-  }
-})
-exit2.addEventListener("click" , (e) => {
-  window.history.pushState(null, "", location.href = "/");
-  window.onpopstate = function(){
-    window.history.pushState(null, "", location.href = "/");
-  }
-})
