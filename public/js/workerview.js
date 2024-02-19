0e7b3df769fbe69fe046959c91b7348058ece1a3
yousef zamzamy <yousefzamzamy1191@gmail.com>
2024-02-19 16:13:41 +0200

    ششش

---
+++ public/js/workerview.js
@@ -0,0 +1,42 @@
+
+const summaryfn = () => {
+  const summary = document.querySelectorAll(".summ");
+  const detTable = document.querySelectorAll(".detTable");
+  summary.forEach((item) => {
+    item.addEventListener("click", (eo) => {
+      const opentab = document.querySelectorAll(".shoow");
+      if (
+        !item.parentElement
+          .getElementsByClassName("detTable")[0]
+          .classList.contains("shoow")
+      ) {
+        item.parentElement
+          .getElementsByClassName("detTable")[0]
+          .classList.toggle("shoow");
+        item
+          .getElementsByClassName("icon-circle-left")[0]
+          .classList.toggle("hide2");
+        item
+          .getElementsByClassName("icon-circle-down")[0]
+          .classList.toggle("show2");
+      }
+
+      opentab.forEach((item) => {
+        item.classList.remove("shoow");
+        item.parentElement
+          .getElementsByClassName("icon-circle-left")[0]
+          .classList.remove("hide2");
+        item.parentElement
+          .getElementsByClassName("icon-circle-down")[0]
+          .classList.remove("show2");
+      });
+    });
+  });
+};
+summaryfn();
+const det = document.getElementById("det");
+det.addEventListener("click", () => {
+  const dettable = document.getElementById("dettable");
+  dettable.classList.toggle("hide2");
+});
+
